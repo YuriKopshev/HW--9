be389322d4f641965bdfff53f0779f9f6a86de57
Yuri Kopshev <yura.kopshev@gmail.com>
2020-08-07 15:52:32 +0200

    added PostManager.class

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,27 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] getPosts() {
+        return posts;
+    }
+
+    public void setPosts(Post[] posts) {
+        this.posts = posts;
+    }
+
+
+    public void wallDelete(Post[]posts) {
+
+    }
+
+    public Post[] wallSearch(Post[]posts) {
+
+        return null;
+    }
+
+
+}
